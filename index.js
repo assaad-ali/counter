934cc0f96544d8c1601dd5cbda7e7ef1411814f6
Ali-Assaad <aliassaadtr@gmail.com>
2023-02-23 21:23:49 +0200

    Create variables

---
+++ index.js
@@ -0,0 +1,8 @@
+const count = document.querySelector('.count');
+const up = document.querySelector('.up');
+const down = document.querySelector('.down');
+const time = document.querySelector('.time');
+const mode = document.querySelector('.mode');
+
+let counter = 0;
+let interval;
